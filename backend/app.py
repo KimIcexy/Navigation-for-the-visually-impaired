4b748f57cfaf93bfefe1b616876475bb08dafab1
KimIcexy <kimicexy2002@gmail.com>
2023-11-10 19:45:00 +0700

    init

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, world!'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
