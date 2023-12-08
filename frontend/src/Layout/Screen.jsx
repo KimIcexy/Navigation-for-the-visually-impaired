ef6a790ee8e08e67d6f2cf2751879982daf450ef
Nguyen Duy Quang <43096050+YumeAyasaki@users.noreply.github.com>
2023-12-09 02:55:50 +0700

    Random things

--- frontend/src/Layout/Screen.jsx
+++ frontend/src/Layout/Screen.jsx
@@ -7,7 +7,6 @@ import { Home, Register, Login, FaceRegister } from "../Screens/";
 const Stack = createStackNavigator();
 
 export default () => {
-
     return (
         <Stack.Navigator>
             <Stack.Screen name="Home" component={Home} />
---
+++ frontend/src/note.md
@@ -0,0 +1,12 @@
+# UI:
+
+Options:
+
+- Voice - Text: React Native Voice, TTS.
+- Gesture: React Native Gesture Handler.
+- Screen Reader.
+
+Things to consider:
+
+- Talkback - Screen reader: Có hỗ trợ tiếng Việt.
+- 
