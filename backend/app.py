5aa4c8e1f204c42231591c7d82eac0ac4171515f
GasTheWoods <124686986+Gia-Huynh@users.noreply.github.com>
2023-12-19 21:40:46 +0700

    Fixing "predict" route

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 import os
 import psycopg2
+import numpy as np
+import base64
+import cv2
 
 from utils.config import *
 from utils.database import Database
@@ -15,6 +18,19 @@ def home():
         return 'Hello, you have connected to the database~~~'
     except Exception as e:
         return str(e)
+
+@app.route('/predict')
+def predict():
+    #Convert base64 image to numpy array
+    data = request.get_json()
+    #You have to skip data:image/jpeg;base64, to get only base64
+    # data which will give you correct image data and cv2 will
+    # decode and display it.
+    header, base64_image = data['image'].split(',', 1)
+    image_bytes = base64.b64decode(base64_image)
+    image_array = np.frombuffer(image_bytes, np.uint8)
+    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    return str(image)
 
 if __name__ == '__main__':
     app.run(debug=True, port=PORT)
