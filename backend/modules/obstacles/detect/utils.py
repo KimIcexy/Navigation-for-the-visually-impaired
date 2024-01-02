77d39ca314cdc2f57083273e117b89499d6d2840
KimIcexy <kimicexy2002@gmail.com>
2024-01-03 04:44:08 +0700

    complete

--- backend/modules/obstacles/detect/utils.py
+++
@@ -1,9 +0,0 @@
-def readClass (filename):
-    with open(filename) as file:
-        lines = [line.rstrip() for line in file]
-    classNum = [int(line.split(': ')[0][1:]) for line in lines]
-    className = [line.split(': ')[1] for line in lines]
-    return classNum, className
-
-if __name__ == "__main__":
-    a, b = readClass ('COCO-Classes_Filtered.txt')
